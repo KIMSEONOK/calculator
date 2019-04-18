729b1b3acfe2ee69d869aa13490ae1f354a83777
KIMSEONOK <tjsdhr0320@naver.com>
2019-04-18 15:51:50 +0900

    oo

---
+++ src/main/java/com/kimso/calculator/Calculator.java
@@ -0,0 +1,14 @@
+package com.kimso.calculator;
+
+import org.springframework.stereotype.Service;
+/**This is calculator service class
+   Seonok Kim
+*/
+@Service
+
+public class Calculator {
+	int sum(int a, int b) {
+		return a+b;
+	}
+
+}
---
+++ src/main/java/com/kimso/calculator/CalculatorApplication.java
@@ -0,0 +1,16 @@
+package com.kimso.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/** This is sample JAVADOC comments
+    Just a Sample
+*/
+@SpringBootApplication
+public class CalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorApplication.class, args);
+	}
+
+}
